821b601359c5bb83de6414f483889c07372546e3
minnnisu <mine52100@naver.com>
2023-12-21 11:20:37 +0900

    ADD: 회원 수정 및 탈퇴 기능 수정

--- public/js/user_update.js
+++ public/js/user_update.js
@@ -131,6 +131,7 @@ function checkRequired(input) {
 // 회원정보 수정
 async function user_update() {
   const targetUpdate = {};
+  let isModified = false;
   const originalUsername =
     document.getElementById("user_info_username").innerText;
   const originalNickname = document
@@ -150,18 +151,26 @@ async function user_update() {
 
   if (usernameUpdateTarget !== originalUsername) {
     targetUpdate.username = usernameUpdateTarget;
+    isModified = true;
   }
 
   if (nicknameUpdateTarget !== originalNickname) {
     targetUpdate.nickname = nicknameUpdateTarget;
+    isModified = true;
   }
 
   if (emailUpdateTarget !== originalEmail) {
     targetUpdate.email = emailUpdateTarget;
+    isModified = true;
   }
 
   if (telephoneUpdateTarget !== originalTelephone) {
     targetUpdate.telephone = telephoneUpdateTarget;
+    isModified = true;
+  }
+
+  if (!isModified) {
+    return alert("수정사항이 없습니다.");
   }
 
   try {
@@ -176,8 +185,8 @@ async function user_update() {
     const data = await response.json();
 
     if (!response.ok) {
-      if (data.message == "not_logout_status_access_error") {
-        return alert("로그인 한 상태에서 접근할 수 없습니다.");
+      if (data.message == "not_login_status_access_error") {
+        return alert("로그아웃 상태에서 접근할 수 없습니다.");
       }
       if (data.message == "nickname_duplication_error") {
         return alert("중복된 닉네임입니다.");
@@ -210,12 +219,14 @@ async function user_delete() {
     });
 
     const data = await response.json();
+
     if (!response.ok) {
       return alert("서버에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.");
     }
+
     close_user_delete_modal();
     open_delete_complete_modal();
   } catch (error) {
-    return alert("알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
+    return alert("예기치 못한 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
   }
 }
